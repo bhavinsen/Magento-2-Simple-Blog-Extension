43e7b8aa0de49025744c8d502c15f70ee76ed803
bhavinsen <senbhavin@gmail.com>
2017-08-05 21:40:13 +0530

    Update UpgradeData.php
    remove error

--- app/code/Bs/Blog/Setup/UpgradeData.php
+++ app/code/Bs/Blog/Setup/UpgradeData.php
@@ -6,7 +6,7 @@ use Magento\Framework\Setup\UpgradeDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 
-class UpgradeData extends UpgradeDataInterface
+class UpgradeData implements UpgradeDataInterface
 {
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
